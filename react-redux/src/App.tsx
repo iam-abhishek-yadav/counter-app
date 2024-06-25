499169b82d1e3fe9ee77ea1790fc2548159ebb12
iam-abhishek-yadav <abhrecscs17@gmail.com>
2024-06-25 22:50:17 +0530

    Add: react-redux/*

---
+++ react-redux/src/App.tsx
@@ -0,0 +1,11 @@
+import Counter from './components/Counter';
+
+const App = () => {
+	return (
+		<div className='min-h-screen bg-gray-400 flex items-center justify-center'>
+			<Counter />
+		</div>
+	);
+};
+
+export default App;
